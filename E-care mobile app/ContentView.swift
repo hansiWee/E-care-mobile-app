957a70b3fa05bd8c6bb0bd513502c7c25764db49
hansiWee <weerathungahasi2@gmail.com>
2024-11-08 10:34:12 +0530

    Initial Commit

---
+++ E-care mobile app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  E-care mobile app
+//
+//  Created by COCOBSCCOMPY4231P-035 on 2024-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ E-care mobile app/E_care_mobile_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_care_mobile_appApp.swift
+//  E-care mobile app
+//
+//  Created by COCOBSCCOMPY4231P-035 on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct E_care_mobile_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
